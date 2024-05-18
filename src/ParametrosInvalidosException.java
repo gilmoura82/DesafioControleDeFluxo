e505ef3843a56d997fe03625d83db81adf4aa1ed
Gil Moura <50829938+gilmoura82@users.noreply.github.com>
2024-05-18 19:33:30 -0300

    Add files via upload

---
+++ src/ParametrosInvalidosException.java
@@ -0,0 +1,14 @@
+
+public class ParametrosInvalidosException extends Exception {
+
+	private static final long serialVersionUID = 1L;
+
+	public ParametrosInvalidosException() {
+		super("O segundo parâmetro deve ser MAIOR que o primeito");
+	}
+	
+
+
+		
+	
+}
